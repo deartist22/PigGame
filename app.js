1f9cc27fea1333d086ef7472b7e12db13907eb2b
deartist22 <deartist22@gmail.com>
2019-03-20 15:52:13 -0500

    Finish init function
    Remove class to reset UI and made Player1 the active player

--- app.js
+++ app.js
@@ -95,4 +95,10 @@ function init() {
 	document.getElementById("name-0").textContent = "Player 1";
 
 	document.getElementById("name-1").textContent = "Player 2";
+
+	document.querySelector(".player-0-panel").classList.remove("winner");
+	document.querySelector(".player-1-panel").classList.remove("winner");
+	document.querySelector(".player-0-panel").classList.remove("active");
+	document.querySelector(".player-1-panel").classList.remove("active");
+	document.querySelector(".player-0-panel").classList.add("active");
 }
