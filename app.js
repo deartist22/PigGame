93d50df1e038b1ccb7a6dbb363abf02e3941100f
deartist22 <deartist22@gmail.com>
2019-03-20 15:41:21 -0500

    Initializing game
    create function to initialize the game using an event listener

--- app.js
+++ app.js
@@ -11,22 +11,9 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
+init();
 
 //document.querySelector("#current-" + activePlayer).textContent = dice;
-
-document.querySelector(".dice").style.display = 'none';
-
-document.getElementById("score-0").textContent = "0";
-
-document.getElementById("score-1").textContent = "0";
-
-document.getElementById("current-0").textContent = "0";
-
-document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
 
@@ -58,7 +45,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
 
 	//Check if player won the game
 	if(scores[activePlayer] >= 10) {
-		document.querySelector("#name-" + activePlayer).textContent = " won!";
+		document.querySelector("#name-" + activePlayer).textContent = "Winner!";
 		document.querySelector(".dice").style.display = "none";
 		document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
 		document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
@@ -85,4 +72,27 @@ function nextPlayer() {
 		document.querySelector(".player-1-panel").classList.toggle("active");
 
 		document.querySelector(".dice").style.display = "none";
+}
+
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+	scores = [0,0];
+	activePlayer = 0;
+	roundScore = 0
+
+	document.querySelector(".dice").style.display = 'none';
+
+	document.getElementById("score-0").textContent = "0";
+
+	document.getElementById("score-1").textContent = "0";
+
+	document.getElementById("current-0").textContent = "0";
+
+	document.getElementById("current-1").textContent = "0";
+
+	document.getElementById("name-0").textContent = "Player 1";
+
+	document.getElementById("name-1").textContent = "Player 2";
 }
