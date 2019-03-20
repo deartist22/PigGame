8a0a33f2e55b4710e26bad2cc7022399f9212edc
deartist22 <deartist22@gmail.com>
2019-03-19 21:12:50 -0500

    Update app.js
    Updating scores and changing players

--- app.js
+++ app.js
@@ -29,9 +29,30 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
+
+	//Random number
 	var dice = Math.floor(Math.random() * 6) + 1;
+	//Display the result
 	var diceDom = document.querySelector(".dice")
 	diceDom.style.display = 'block';
 	diceDom.src = "dice-" + dice + ".png";
+
+	if(dice !== 1) {
+		//Add score
+		roundScore += dice;
+		document.querySelector("#current-" + activePlayer).textContent = roundScore;
+	} else {
+		//Next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById("current-0").textContent = '0';
+		document.getElementById("current-1").textContent = '0';
+
+		document.querySelector(".player-0-panel").classList.toggle("active");
+		document.querySelector(".player-1-panel").classList.toggle("active");
+
+		document.querySelector(".dice").style.display = "none";
+	}
 
 })
